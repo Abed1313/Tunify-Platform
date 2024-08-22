ad5b88f1a28d0ead5e5db455f754546e42370639
AbdelRadwan <21023979@student.ltuc.com>
2024-08-23 01:33:30 +0300

    Tunify Platform - Identity Implementation

---
+++ Tunify-Platform/Repositories/Interfaces/IUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Tunify_Platform.Models.DTO;
+
+namespace Tunify_Platform.Repositories.Interfaces
+{
+    public interface IUser
+    {
+        //Add regester
+        Task<UserDto> Register(RegisterUserDTO registerEmployeeDTO, ModelStateDictionary modelState);
+
+        // Add Login 
+        public Task<UserDto> LoginUser(string Username , string Password);
+
+        public Task<UserDto> LogoutUser(string Username);
+    }
+}
